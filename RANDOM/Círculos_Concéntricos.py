beecbc14b001d2e9b3e445abb84cab8ee1047d63
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:35:27 -0600

    Update Círculos_Concéntricos.py

--- RANDOM/Círculos_Concéntricos.py
+++ RANDOM/Círculos_Concéntricos.py
@@ -7,3 +7,10 @@ t = np.linspace(0, 2 * np.pi, 1000)
 # Definir las funciones paramétricas para x(t) y y(t) - circunferencia exterior
 x_exterior = np.cos(t)
 y_exterior = np.sin(t)
+
+# Definir el rango de parámetro t para los círculos interiores
+t_interior = np.linspace(0, 2 * np.pi, 1000)
+
+# Definir las funciones paramétricas para x(t) y y(t) - círculo interior 1
+x_interior1 = 0.5 * np.cos(t_interior)  # Radio 0.5
+y_interior1 = 0.5 * np.sin(t_interior)
