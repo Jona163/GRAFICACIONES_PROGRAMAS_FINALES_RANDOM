114cfb5a7167a260c37534cfb29dfac5443f3f96
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:35:15 -0600

    Update Círculos_Concéntricos.py

--- RANDOM/Círculos_Concéntricos.py
+++ RANDOM/Círculos_Concéntricos.py
@@ -1,1 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Definir el rango de parámetro t para la circunferencia exterior
+t = np.linspace(0, 2 * np.pi, 1000)
+
+# Definir las funciones paramétricas para x(t) y y(t) - circunferencia exterior
+x_exterior = np.cos(t)
+y_exterior = np.sin(t)
