95661fcef2e18eac5ba634c894cd190f4f208613
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:32:51 -0600

    Update ANIMACION.py

--- RANDOM/ANIMACION.py
+++ RANDOM/ANIMACION.py
@@ -2,3 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.optimize import linprog
+
+# Coeficientes de la función objetivo
+c = [-25, -30]
+
+# Coeficientes de las restricciones
+A = [[1, 1.5], [1.5, 1]]
+b = [750, 750]
+
+# Límites de las variables
+x_bounds = (0, None)
+y_bounds = (0, None)
