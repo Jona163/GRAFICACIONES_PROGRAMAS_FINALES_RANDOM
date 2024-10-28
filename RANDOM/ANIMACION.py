9f593fbe1fdafd8dcbdb0d8e66074dbbce31fd88
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:33:04 -0600

    Update ANIMACION.py

--- RANDOM/ANIMACION.py
+++ RANDOM/ANIMACION.py
@@ -13,3 +13,10 @@ b = [750, 750]
 # Límites de las variables
 x_bounds = (0, None)
 y_bounds = (0, None)
+
+# Resolución del problema de programación lineal
+result = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds], method='simplex')
+
+# Valores iniciales de las variables
+x_initial = 320  # Cantidad de joyas de tipo A
+y_initial = 320  # Cantidad de joyas de tipo B
