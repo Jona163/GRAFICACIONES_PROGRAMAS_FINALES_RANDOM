861c96b0f74976a99d2df417bcc20d10575ae946
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:37:29 -0600

    Update region_factible.py

--- RANDOM/region_factible.py
+++ RANDOM/region_factible.py
@@ -1,1 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Definir las restricciones
+x = np.linspace(0, 10, 100)  # Valores de A en el rango [0, 10]
+y1 = (12 - x) / 2  # Restricción 1: A + 2B > 12
+y2 = 10 - 2*x  # Restricción 2: 2A + B > 10
