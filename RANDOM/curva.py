630a8518f988ba22981aa0c4fedade5b007f18c1
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:41:37 -0600

    Update curva.py

--- RANDOM/curva.py
+++ RANDOM/curva.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 
 # Definir el rango del parametro t
 t=np.linspace(0, 2 * np.pi, 1000)
+
+# Definir las funciones de x y y
+x = np.cos(t)
+y = np.sin(t)
+
+# Crear Grafica
+plt.figure(figsize=(6, 6))
+plt.plot(x, y, label='Circunferencia Parametrica')
+plt.axis('equal')
