283f89e1c84af62bed78453b361cd39230a8fa16
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:40:12 -0600

    Update Codigo_simplex.py

--- RANDOM/Codigo_simplex.py
+++ RANDOM/Codigo_simplex.py
@@ -8,3 +8,15 @@ c = [-25, -30]  # Coeficientes de x y y en la función objetivo (se multiplican 
 # Coeficientes de las restricciones (lado izquierdo de las desigualdades)
 A = [[1, 1.5], [1.5, 1]]  # Coeficientes de x y y en las restricciones
 b = [750, 750]  # Lado derecho de las restricciones (disponibilidad de oro y plata)
+
+# Límites de las variables
+x_bounds = (0, None)  # x >= 0
+y_bounds = (0, None)  # y >= 0
+
+# Resolución del problema de programación lineal
+result = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds], method='simplex')
+
+# Extraer los resultados
+x = result.x[0]  # Cantidad de joyas de tipo A
+y = result.x[1]  # Cantidad de joyas de tipo B
+max_beneficio = -result.fun  # Máximo beneficio
