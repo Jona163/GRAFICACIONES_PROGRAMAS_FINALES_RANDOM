d016d1818a58a09b945d4e43b643e2e737c69e9c
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:40:01 -0600

    Update Codigo_simplex.py

--- RANDOM/Codigo_simplex.py
+++ RANDOM/Codigo_simplex.py
@@ -1,1 +1,10 @@
+import numpy as np
+from scipy.optimize import linprog
+import matplotlib.pyplot as plt
 
+# Coeficientes de la función objetivo
+c = [-25, -30]  # Coeficientes de x y y en la función objetivo (se multiplican por -1 para convertir en maximización)
+
+# Coeficientes de las restricciones (lado izquierdo de las desigualdades)
+A = [[1, 1.5], [1.5, 1]]  # Coeficientes de x y y en las restricciones
+b = [750, 750]  # Lado derecho de las restricciones (disponibilidad de oro y plata)
