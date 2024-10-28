b2b268628be868c416ba08128273dd7ccd8b818a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-28 11:33:21 -0600

    Update ANIMACION.py

--- RANDOM/ANIMACION.py
+++ RANDOM/ANIMACION.py
@@ -20,3 +20,15 @@ result = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds], method='simplex
 # Valores iniciales de las variables
 x_initial = 320  # Cantidad de joyas de tipo A
 y_initial = 320  # Cantidad de joyas de tipo B
+
+# Valores objetivo óptimos
+x_optimal = result.x[0]  # Cantidad óptima de joyas de tipo A
+y_optimal = result.x[1]  # Cantidad óptima de joyas de tipo B
+
+# Configuración de la gráfica de barras
+fig, ax = plt.subplots()
+bar_labels = ['Joyas A', 'Joyas B']
+bar_values = [x_initial, y_initial]
+bars = ax.bar(bar_labels, bar_values)
+ax.set_ylabel('Cantidad')
+ax.set_title('Cantidad de joyas de cada tipo')
