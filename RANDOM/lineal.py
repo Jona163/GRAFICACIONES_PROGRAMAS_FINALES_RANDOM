fcc0f0a5bffc6df46c0ca8c64ee3778b1e68fe27
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-29 11:06:07 -0600

    Update lineal.py

--- RANDOM/lineal.py
+++ RANDOM/lineal.py
@@ -1,1 +1,8 @@
+from matplotlib import pyplot
 
+#Función Lineal.
+def f(x):
+    return 4*x-7
+#En esta variable se genera una lista con valores del -10 al 10.
+#Todos estos valores serán los que tomara x.
+x = range(-10, 10)
