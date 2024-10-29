7af375e0567c1afe31071b26b446f4817c50b356
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-29 11:08:16 -0600

    Update Polinomial.py

--- RANDOM/Polinomial.py
+++ RANDOM/Polinomial.py
@@ -3,3 +3,8 @@ import numpy as np
 import animatplot as amp
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
+########## PREPARAR LA DATA ##########
+#Importamos los datos de la misma librería de scikit-learn
+boston = datasets.load_boston()
+print(boston)
+print()
