5409c425b2c7510bf2209c284bfc562f11023cd3
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-29 11:14:30 -0600

    Update funcion_cuadratica.py

--- RANDOM/funcion_cuadratica.py
+++ RANDOM/funcion_cuadratica.py
@@ -9,3 +9,9 @@ c=float(input("ingrese el valor de (c):\n"))
 
 disc=(b**2)-(4*a*c)
 raiz=(disc)**(0.5)
+
+if(disc>0):
+    x1=((-b)+raiz)/(2*a)
+    x2=((-b)-raiz)/(2*a)
+    print("x1= ",x1)
+    print("x2= ",x2)
