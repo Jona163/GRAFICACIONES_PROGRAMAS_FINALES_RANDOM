2d1d99aae1d4a1c32e8e9efbbc90de45057085ca
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-29 11:15:10 -0600

    Update funcion_cuadratica.py

--- RANDOM/funcion_cuadratica.py
+++ RANDOM/funcion_cuadratica.py
@@ -10,6 +10,7 @@ c=float(input("ingrese el valor de (c):\n"))
 disc=(b**2)-(4*a*c)
 raiz=(disc)**(0.5)
 
+
 if(disc>0):
     x1=((-b)+raiz)/(2*a)
     x2=((-b)-raiz)/(2*a)
@@ -23,3 +24,21 @@ elif(disc==0):
     print("x2= ",x2)
 else:
     print("Solucion imaginaria")
+
+
+
+def f(x):
+    return a*(x**2)+b*x+c
+
+x = np.linspace(-100,50+np.pi,100)
+
+pyplot.plot(x,[f(i)for i in x])
+
+pyplot.axhline(0, color= "black")
+pyplot.axvline(0, color= "black")
+
+pyplot.xlim(-50,50)
+pyplot.ylim(-50,50)
+
+pyplot.savefig("output.png")
+pyplot.show()
