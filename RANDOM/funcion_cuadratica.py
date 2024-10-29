5a4114e8bdf7f8b7ad5e98181cf1bfa412c28a38
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-29 11:14:08 -0600

    Update funcion_cuadratica.py

--- RANDOM/funcion_cuadratica.py
+++ RANDOM/funcion_cuadratica.py
@@ -2,5 +2,7 @@
 import math as m
 import numpy as np
 from matplotlib import pyplot
+
 a=float(input("ingrese el valor de (a):\n"))
 b=float(input("ingrese el valor de (b):\n"))
+c=float(input("ingrese el valor de (c):\n"))
